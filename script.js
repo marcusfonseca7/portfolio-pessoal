85ad876df2145220536b466c6dbb30a70e7b8c62
Marcus <marcus.fonseca968@gmail.com>
2026-04-09 19:55:25 -0300

    style: melhoria na animação com js e css

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const description = document.getElementById("projectDescription");
 const containerProject = document.getElementById("project");
 
 function openDescription() {
-  description.classList.toggle("hidden");
   containerProject.classList.toggle("active");
+  description.classList.toggle("hidden");
 }
