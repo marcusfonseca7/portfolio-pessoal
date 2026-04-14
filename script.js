f9dae0be38bfb6d74611a3b860e328b7edaff089
Marcus <marcus.fonseca968@gmail.com>
2026-04-14 19:15:27 -0300

    feat: modo claro e escuro

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const containerProject = document.querySelectorAll(".projects__project");
 const description = document.querySelectorAll(".projects__description");
 const image = document.querySelectorAll(".projects__image-project");
+const root = document.documentElement;
+const imageMode = document.getElementById("mode");
 
 containerProject.forEach((project, index) => {
   image[index].addEventListener("click", () => {
@@ -8,3 +10,13 @@ containerProject.forEach((project, index) => {
     description[index].classList.toggle("hidden");
   });
 });
+
+function turnMode() {
+  root.classList.toggle("light");
+  if(root.className === "light"){
+    imageMode.setAttribute('src', 'img/modoescuro.png')
+  } else {
+    imageMode.setAttribute('src', 'img/modoclaro.png')
+
+  }
+} 
